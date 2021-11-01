6e29d37674bcaccf448703b6d79c72591b8b4bca
ranchordo <ranchordo@gmail.com>
2021-11-01 13:40:23 -0600

    Update poly-generator.py

--- poly-generator.py
+++ poly-generator.py
@@ -3,7 +3,9 @@ import numpy as np
 import os
 
 GRAD_DELTA=3
-GRAD_RATE=1000*0.17
+CUT_GRAD_DELTA=0.01
+GRAD_RATE=0.17
+CUT_GRAD_RATE=0.0002
 
 class Triangle:
     def __init__(self,xa,ya,xb,yb,xc,yc,img):
@@ -18,15 +20,16 @@ class Triangle:
         self.mask=np.full(self.img.shape,0,np.uint8)
         cv2.drawContours(self.mask,self.ctr,-1,(255,255,255),-1)
         self.mask=cv2.cvtColor(self.mask,cv2.COLOR_BGR2GRAY)
-        self.maskBGR=cv2.cvtColor(self.mask,cv2.COLOR_GRAY2BGR)
+        #self.maskBGR=cv2.cvtColor(self.mask,cv2.COLOR_GRAY2BGR)
         self.msum=np.sum(self.mask)/255.
         self.mmsum=self.img.shape[0]*self.img.shape[1]
         self.center=self.getCenter()
         self.invalid=(xa<0) or (ya<0) or (xb<0) or (yb<0) or (xc<0) or (yc<0)
+        self.error=-10
         if self.invalid:
             print("Recieved an invalid triangle. Ignoring...")
-        self.mean=cv2.mean(self.img,mask=self.mask)
-        #self.mean=self.img[int(self.center[1]),int(self.center[0])]
+        #self.mean=cv2.mean(self.img,mask=self.mask)
+        self.mean=self.img[min(int(self.center[1]),self.img.shape[0]-1),min(int(self.center[0]),self.img.shape[1]-1)]
     def draw(self,drimg):
         if self.invalid: return
         cv2.drawContours(drimg,self.ctr,-1,(int(self.mean[0]),int(self.mean[1]),int(self.mean[2])),-1)
@@ -35,12 +38,18 @@ class Triangle:
     def drawErrors(self,drimg):
         cv2.drawContours(drimg,self.ctr,-1,(self.getError(),0,0),-1)
     def getError(self):
+        if self.error!=-10:
+            return self.error
         if self.invalid or self.msum<=1000:
             return -1
         mimg=np.full(self.img.shape,self.mean[0:3],np.uint8)
         dimg=cv2.absdiff(mimg, self.img)
-        m=np.array(cv2.mean(dimg,mask=self.mask))
-        return np.sqrt(np.abs(m.dot(m)))+(250*(self.msum/self.mmsum))
+        dimg=cv2.cvtColor(dimg,cv2.COLOR_BGR2GRAY)
+        mdimg=dimg*self.mask
+        m=cv2.mean(dimg,mask=self.mask)[0]
+        mx=np.array(mdimg).max()
+        self.error=m+(1.5*mx)+(250*(self.msum/self.mmsum))
+        return self.error
     def checkPointWithin(self,p):
         c1=(self.xb-self.xa)*(p[1]-self.ya)-(self.yb-self.ya)*(p[0]-self.xa)
         c2=(self.xc-self.xb)*(p[1]-self.yb)-(self.yc-self.yb)*(p[0]-self.xb)
@@ -74,38 +83,58 @@ class Triangle:
             err_y=self.getPhantomSubdivisionErrorMax(guessy)
             dx=(err_x-err_base)/(GRAD_DELTA*1.)
             dy=(err_y-err_base)/(GRAD_DELTA*1.)
-            guess=(guess[0]-(GRAD_RATE*dx),guess[1]-(GRAD_RATE*dy))
-            if iterations>=5 or not self.checkPointWithin(guess):
-                break;
-            if guess[0]<0 or guess[0]>self.img.shape[0] or guess[1]<0 or guess[1]>self.img.shape[0]:
-                break;
             if err_base<bestError:
                 bestGuess=guess
                 bestError=err_base
                 iterations=0
             else:
                 iterations+=1
-            #print(dx,dy,guess,err_base,self.checkPointWithin(guess))
-        #os._exit(0)
+            guess=(guess[0]-(GRAD_RATE*dx*self.img.shape[1]),guess[1]-(GRAD_RATE*dy*self.img.shape[0]))
+            if iterations>=5 or not self.checkPointWithin(guess):
+                break
+            if guess[0]<0 or guess[0]>self.img.shape[0] or guess[1]<0 or guess[1]>self.img.shape[0]:
+                break
+        return bestGuess
+    def minimalErrorCutDeltaRatio(self):
+        guess=0.5
+        bestGuess=guess
+        bestError=self.img.shape[0]*self.img.shape[1]*255
+        iterations=0
+        while True:
+            guessd=guess+CUT_GRAD_DELTA
+            err_base=self.getPhantomSubdivisionErrorMax_cut(guess)
+            err_d=self.getPhantomSubdivisionErrorMax_cut(guessd)
+            dedp=(err_d-err_base)/(CUT_GRAD_DELTA*1.)
+            if iterations>=5:
+                break
+            if guess>=1 or guess<=0:
+                break
+            if err_base<bestError:
+                bestGuess=guess
+                bestError=err_base
+                iterations=0
+            else:
+                iterations+=1
+            guess-=CUT_GRAD_RATE*dedp
         return bestGuess
     def minimalErrorSubdivisionInfo(self):
         point=self.minimalErrorSubdivisionPoint()
+        cutp=self.minimalErrorCutDeltaRatio()
         err_point=self.getPhantomSubdivisionErrorMax(point)
-        err_cut=self.getPhantomSubdivisionErrorMax_cut()
-        return (subdivide_point if err_point<err_cut else subdivide_cut),point
+        err_cut=self.getPhantomSubdivisionErrorMax_cut(cutp)
+        return (subdivide_point if err_point<err_cut else subdivide_cut),(point if err_point<err_cut else cutp)
     def getPhantomSubdivisionErrorMax(self, p):
         nl=[]
         subdivide_point([self],0,p,nl=nl)
         errors=[tr.getError() for tr in nl]
-        return np.max(errors) if len(errors)>0 else -1
-    def getPhantomSubdivisionErrorMax_cut(self):
+        return getMultiCost(errors)
+    def getPhantomSubdivisionErrorMax_cut(self,p):
         nl=[]
-        subdivide_cut([self],0,0,nl=nl)
+        subdivide_cut([self],0,p,nl=nl)
         errors=[tr.getError() for tr in nl]
-        return np.max(errors)*0.85 if len(errors)>0 else -1
-def createTriangleContour(t):
-    L=[[t.xa,t.ya],[t.xb,t.yb],[t.xc,t.yc]]
-    return np.array(L).reshape((-1,1,2)).astype(np.int32)
+        return 0.85*getMultiCost(errors)
+def getMultiCost(errors):
+    return np.mean(errors) if len(errors)>0 else -1
 def drawTris(ts,img):
     for t in ts:
         if t is not None:
@@ -113,15 +142,21 @@ def drawTris(ts,img):
 def append(nl,tri):
     if not tri.invalid:
         nl.append(tri)
+def palongSeg(a, b, p):
+    dx=b[0]-a[0]
+    dy=b[1]-a[1]
+    dist=np.sqrt(np.power(b[0]-a[0],2)+np.power(b[1]-a[1],2))
+    return (a[0]+(p*dx),a[1]+(p*dy))
+    
 def subdivide_cut(ts,i,p,nl=None):
     if nl is None:
         nl=ts
-    mABx=(ts[i].xa+ts[i].xb)/2.0
-    mABy=(ts[i].ya+ts[i].yb)/2.0
-    mBCx=(ts[i].xb+ts[i].xc)/2.0
-    mBCy=(ts[i].yb+ts[i].yc)/2.0
-    mACx=(ts[i].xc+ts[i].xa)/2.0
-    mACy=(ts[i].yc+ts[i].ya)/2.0
+    a=(ts[i].xa,ts[i].ya)
+    b=(ts[i].xb,ts[i].yb)
+    c=(ts[i].xc,ts[i].yc)
+    mABx, mABy=palongSeg(a, b, p)
+    mBCx, mBCy=palongSeg(b, c, p)
+    mACx, mACy=palongSeg(c, a, p)
     append(nl,Triangle(ts[i].xa,ts[i].ya,mABx,mABy,mACx,mACy,ts[i].img))
     append(nl,Triangle(ts[i].xb,ts[i].yb,mABx,mABy,mBCx,mBCy,ts[i].img))
     append(nl,Triangle(ts[i].xc,ts[i].yc,mACx,mACy,mBCx,mBCy,ts[i].img))
@@ -139,14 +174,14 @@ def subdivide_point(ts, i, p, nl=None):
     ts[i]=None
     
 
-img=cv2.imread(r'C:/Users/ranch/Downloads/dino.jpg')
+img=cv2.imread(r'/home/ranchordo/Desktop/keyboard.jpg')
 img=cv2.GaussianBlur(img,(11,11),cv2.BORDER_DEFAULT)
 #img=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
 tris=[Triangle(0,0,img.shape[1],0,0,img.shape[0],img),Triangle(img.shape[1],img.shape[0],img.shape[1],0,0,img.shape[0],img)]
 drimg=np.zeros(img.shape,np.uint8)
 blimg=None
-alpha=0.93
+alpha=0.8
 while True:
     maxidx=-1
     err=-1
@@ -155,6 +190,7 @@ while True:
         if e>err:
             err=e
             maxidx=i
+    maxtri=tris[maxidx]
     subdiv_func,subdiv=tris[maxidx].minimalErrorSubdivisionInfo()
     subdiv_func(tris,maxidx,subdiv)
     nl=[]
@@ -162,15 +198,17 @@ while True:
         if tr is not None:
             if not tr.invalid:
                 nl.append(tr)
+    otris=tris
     tris=nl
     drawTris(tris,drimg)
-    tris[maxidx].drawOutline(drimg)
+    maxtri.drawOutline(drimg)
     blimg=cv2.addWeighted(drimg,alpha,img,1-alpha,0.0)
     cv2.imshow('tris',blimg)
     if cv2.waitKey(10)==27:
         cv2.destroyAllWindows()
         break
 drawTris(tris,drimg)
+alpha=1
 blimg=cv2.addWeighted(drimg,alpha,img,1-alpha,0.0)
 cv2.imshow('tris',blimg)
-cv2.waitKey(50)
+cv2.waitKey(0)
